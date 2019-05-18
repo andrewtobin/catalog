e4febf8e43f23abec7cdcb60c73741c83137fdc4
Andrew Tobin <andrew@grrargh.com>
2019-05-18 13:21:04 +1000

    Added some basic scaffolding in Catalog.Data

---
+++ Catalog.Data/CatalogDataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Catalog.Data
+{
+    public class CatalogDataContext: DbContext
+    {
+
+    }
+}
